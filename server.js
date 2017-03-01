12e063904ec4b1ca8557c268c21dc1b7c197090c
Tansel Kılıç <tanselkilic@gmail.com>
2017-03-01 14:38:25 +0300

    server.js file updated

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require("express");
 var app = express();
 
 app.get("/", function (req, resp) {
-    resp.send("<h1>Loading basic HTML in Node.js - 2</h1>"); 
+    resp.send("<h1>Merhaba Dünya - 3</h1>"); 
 });
 
 app.listen(8182); //uygulamamız bu portu dinleyecek. http://localhost:8182
